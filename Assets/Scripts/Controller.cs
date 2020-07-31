328030552d4f8245c88254f4164aad857c716c3d
mark-sawyer <alfador_321@hotmail.com>
2020-07-31 19:55:33 +1000

    Added speed changing, instant colour changing, escape to quit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour {
+    public static int HORIZONTAL_PIXELS = 300;
+    public static int VERTICAL_PIXELS = 150;
+    public static GameObject[,] pixels;
+    public GameObject pixel;
+    public GameObject selectionBox;
+    public GameObject radiusText;
+    public GameObject timerText;
+    private int MIN_TIMER_RESET = 1;
+    private int MAX_TIMER_RESET = 100;
+    private float timerReset;
+    private float timer;
+    private float clickRadius;
+    private float MAX_RADIUS = 20f;
+    private float MIN_RADIUS = 0.5f;
+    private bool isRadiusMode;
+    public static selectedColour colour = selectedColour.RED;
+
+    void Start() {
+        isRadiusMode = true;
+        timerReset = MIN_TIMER_RESET;
+        timer = MIN_TIMER_RESET / 20;
+        clickRadius = 5f;
+        radiusText.GetComponent<Text>().text = string.Format("{0:N1}", clickRadius);
+        timerText.GetComponent<Text>().text = "0.05";
+        radiusText.GetComponent<Text>().color = Color.red;
+        selectionBox.GetComponent<SpriteRenderer>().color = Color.red;
+
+        pixels = new GameObject[HORIZONTAL_PIXELS, VERTICAL_PIXELS];
+        for (int row = 0; row < HORIZONTAL_PIXELS; row++) {
+            for (int col = 0; col < VERTICAL_PIXELS; col++) {
+                pixels[row, col] = Instantiate(pixel, new Vector3(row, col, 0), UnityEngine.Quaternion.identity);
+                pixels[row, col].GetComponent<RGB>().setPosition(row, col);
+            }
+        }
+
+        for (int row = 0; row < HORIZONTAL_PIXELS; row++) {
+            for (int col = 0; col < VERTICAL_PIXELS; col++) {
+                pixels[row, col].GetComponent<RGB>().setNeighbours();
+            }
+        }
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Application.Quit();
+        }
+
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            timer = timerReset / 20;
+
+            for (int row = 0; row < HORIZONTAL_PIXELS; row++) {
+                for (int col = 0; col < VERTICAL_PIXELS; col++) {
+                    pixels[row, col].GetComponent<RGB>().setNeighboursUpdate();
+                }
+            }
+
+            for (int row = 0; row < HORIZONTAL_PIXELS; row++) {
+                for (int col = 0; col < VERTICAL_PIXELS; col++) {
+                    pixels[row, col].GetComponent<RGB>().resolveColours();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown("space")) {
+            switch (colour) {
+                case selectedColour.RED:
+                    selectionBox.GetComponent<SpriteRenderer>().color = Color.green;
+                    colour = selectedColour.GREEN;
+                    break;
+                case selectedColour.GREEN:
+                    selectionBox.GetComponent<SpriteRenderer>().color = Color.blue;
+                    colour = selectedColour.BLUE;
+                    break;
+                case selectedColour.BLUE:
+                    selectionBox.GetComponent<SpriteRenderer>().color = Color.white;
+                    colour = selectedColour.WHITE;
+                    break;
+                case selectedColour.WHITE:
+                    selectionBox.GetComponent<SpriteRenderer>().color = Color.red;
+                    colour = selectedColour.RED;
+                    break;
+            }
+        }
+
+        if (Input.GetMouseButton(0)) {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Collider2D[] clickSpot = Physics2D.OverlapCircleAll(mousePos, clickRadius);
+            for (int i = 0; i < clickSpot.Length; i++) {
+                clickSpot[i].GetComponent<RGB>().manuallyChangeColour();
+            }
+        }
+
+        if (Input.mouseScrollDelta.y != 0) {
+            scrollHandling(Input.mouseScrollDelta.y);
+        }
+
+        if (Input.GetKeyDown("return")) {
+            for (int row = 0; row < HORIZONTAL_PIXELS; row++) {
+                for (int col = 0; col < VERTICAL_PIXELS; col++) {
+                    pixels[row, col].GetComponent<RGB>().resetColour();
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.Tab)) {
+            if (isRadiusMode) {
+                radiusText.GetComponent<Text>().color = Color.black;
+                timerText.GetComponent<Text>().color = Color.red;
+                isRadiusMode = false;
+            }
+            else {
+                radiusText.GetComponent<Text>().color = Color.red;
+                timerText.GetComponent<Text>().color = Color.black;
+                isRadiusMode = true;
+            }
+        }
+    }
+
+    public void scrollHandling(float dir) {
+        if (isRadiusMode) {
+            if (dir > 0) {
+                if (clickRadius < MAX_RADIUS) {
+                    clickRadius += MIN_RADIUS;
+                    radiusText.GetComponent<Text>().text = string.Format("{0:N1}", clickRadius);
+                }
+            }
+            else if (dir < 0) {
+                if (clickRadius > MIN_RADIUS) {
+                    clickRadius -= MIN_RADIUS;
+                    radiusText.GetComponent<Text>().text = string.Format("{0:N1}", clickRadius);
+                }
+            }
+        }
+        else {
+            if (dir > 0) {
+                if (timerReset < MAX_TIMER_RESET) {
+                    timerReset += MIN_TIMER_RESET;
+                    timerText.GetComponent<Text>().text = string.Format("{0:N2}", timerReset / 20);
+                }
+            }
+            else if (dir < 0) {
+                if (timerReset > MIN_TIMER_RESET) {
+                    timerReset -= MIN_TIMER_RESET;
+                    timerText.GetComponent<Text>().text = string.Format("{0:N2}", timerReset / 20);
+                }
+            }
+        }
+    }
+}
+
+public enum selectedColour {
+    RED,
+    GREEN,
+    BLUE,
+    WHITE
+};
---
+++ Assets/Scripts/RGB.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RGB : MonoBehaviour {
+    public SpriteRenderer sprite;
+    public static bool round1;
+    public bool red;
+    public bool green;
+    public bool blue;
+    public bool redUpdate;
+    public bool greenUpdate;
+    public bool blueUpdate;
+    public int x;
+    public int y;
+    private GameObject north;
+    private GameObject east;
+    private GameObject south;
+    private GameObject west;
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setNeighbours() {
+        // Set north
+        if (y != Controller.VERTICAL_PIXELS - 1) {
+            north = Controller.pixels[x, y + 1];
+        }
+        // Set east
+        if (x != Controller.HORIZONTAL_PIXELS - 1) {
+            east = Controller.pixels[x + 1, y];
+        }
+        // Set south
+        if (y != 0) {
+            south = Controller.pixels[x, y - 1];
+        }
+        // Set west
+        if (x != 0) {
+            west = Controller.pixels[x - 1, y];
+        }
+    }
+
+    public void setNeighboursUpdate() {
+        if(red) {
+            redUpdate = true;
+        }
+        else if (green) {
+            greenUpdate = true;
+        }
+        else if (blue) {
+            blueUpdate = true;
+        }
+        setUpdate(north);
+        setUpdate(east);
+        setUpdate(south);
+        setUpdate(west);
+    }
+
+    public void setUpdate(GameObject neighbour) {
+        if (neighbour != null) {
+            if (red) {
+                neighbour.GetComponent<RGB>().redUpdate = true;
+            }
+            else if (green) {
+                neighbour.GetComponent<RGB>().greenUpdate = true;
+            }
+            else if (blue) {
+                neighbour.GetComponent<RGB>().blueUpdate = true;
+            }
+        }
+    }
+
+    public void resolveColours() {
+        if (redUpdate & greenUpdate & blueUpdate) {
+            red = false;
+            green = false;
+            blue = false;
+        }
+        else if (redUpdate & greenUpdate) {
+            red = true;
+            green = false;
+            blue = false;
+        }
+        else if (greenUpdate & blueUpdate) {
+            red = false;
+            green = true;
+            blue = false;
+        }
+        else if (blueUpdate & redUpdate) {
+            red = false;
+            green = false;
+            blue = true;
+        }
+        else if (redUpdate) {
+            red = true;
+            green = false;
+            blue = false;
+        }
+        else if (greenUpdate) {
+            red = false;
+            green = true;
+            blue = false;
+        }
+        else if (blueUpdate) {
+            red = false;
+            green = false;
+            blue = true;
+        }
+
+        if (red) {
+            sprite.color = Color.red;
+        }
+        else if (green) {
+            sprite.color = Color.green;
+        }
+        else if (blue) {
+            sprite.color = Color.blue;
+        }
+        else {
+            sprite.color = Color.white;
+        }
+
+        redUpdate = false;
+        greenUpdate = false;
+        blueUpdate = false;
+    }
+
+    public void manuallyChangeColour() {
+        switch (Controller.colour) {
+            case selectedColour.RED:
+                red = true;
+                green = false;
+                blue = false;
+                sprite.color = Color.red;
+                break;
+            case selectedColour.GREEN:
+                red = false;
+                green = true;
+                blue = false;
+                sprite.color = Color.green;
+                break;
+            case selectedColour.BLUE:
+                red = false;
+                green = false;
+                blue = true;
+                sprite.color = Color.blue;
+                break;
+            case selectedColour.WHITE:
+                red = false;
+                green = false;
+                blue = false;
+                sprite.color = Color.white;
+                break;
+        }
+
+        redUpdate = false;
+        greenUpdate = false;
+        blueUpdate = false;
+    }
+
+    public void resetColour() {
+        red = false;
+        green = false;
+        blue = false;
+        redUpdate = false;
+        greenUpdate = false;
+        blueUpdate = false;
+    }
+}
